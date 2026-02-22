dd35730a2f823086b83f7033d2cde74593e4c4f7
Salva <salvador.martos@gmail.com>
2026-02-22 11:36:05 +0100

    El precio debe ser mayor de 0

--- app/schemas.py
+++ app/schemas.py
@@ -2,8 +2,7 @@ from pydantic import Field, BaseModel, ConfigDict # Importa ConfigDict
 
 class ProductoBase(BaseModel):
     descripcion: str = Field(min_length=3, max_length=100)
-    #precio: float = Field(gt=0, description="El precio debe ser mayor que cero")
-    precio: float = Field()
+    precio: float = Field(gt=0, description="El precio debe ser mayor que cero")
 
 # Para crear un producto
 class ProductoCreate(ProductoBase):
